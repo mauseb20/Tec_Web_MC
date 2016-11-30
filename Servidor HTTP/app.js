7703d2052d29cd4306a3e44e90801106f85fda10
mauseb20 <mauseb91@gmail.com>
2016-11-30 14:58:16 -0500

    Crear y consultar

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var app = express();
 
+var usuarios = [
+    {
+        id:1,
+        nombre:'Pepe',
+        cedula:'1254589667'
+    },
+    {
+        id:2,
+        nombre:'carlos',
+        cedula:'1712548879'
+    },
+    {
+        id:3,
+        nombre:'Juan',
+        cedula:'9999999999'
+    }
+]
+var contador =3;
+
 app.get('/', function (req, res) {
   res.send('Hello World!!');
 });
@@ -9,11 +28,41 @@ app.get('/TecnologiasWeb', function (req, res) {
   res.send('con JavaScript!!');
 });
 
-//app.post('/TecnologiasWeb', function (req, res) {
-  //res.send('con post!!');
-//});
+app.get('/Usuarios', function (req, res) {
+  res.json(usuarios);
+});
 
-app.post('/TecnologiasWeb', function (req, res) {
+app.get('/Usuario/:idUsuario', function (req, res) {
+    var idActual = req.params.idUsuario;
+    for (var i=0;i<usuarios.length;i++){
+        if (idActual==usuarios[i].id){
+            res.json(usuarios[i]);
+        }  
+    }
+    res.send('No existe el usuario');
+});
+
+app.post('/Usuario', function (req, res) {
+    if(!req.query.nombre){
+        res.send('No envio el nombre');
+    }
+    if(!req.query.cedula){
+        res.send('No envio la cedula');
+    }
+    var nuevoUsuario={
+        id:contador+1,
+        nombre:req.query.nombre,
+        cedula:req.query.cedula
+    };
+    usuarios.push(nuevoUsuario);
+    contador++;
+    res.json(nuevoUsuario);
+    console.log(req.query.nombre);
+    console.log(req.query.cedula);
+    
+});
+
+app.post('/TecnologiasWeb:nombre', function (req, res) {
   //res.send('con post!!');
     
     var parametros = req.params;
@@ -25,10 +74,6 @@ app.post('/TecnologiasWeb', function (req, res) {
     //res.headers.token=1234;
     //res.headers.usuario=usuario.nombre;
     res.append('token','1234');
-    
-    usuario.apellido='Cabrera';
-    usuario.mascotas=[];
-    usuario.casado=false;
     
     res.json (usuario);
     
