2cb308afeb7082aed8f4a4adf156a93c393e3761
mauseb20 <mauseb91@gmail.com>
2017-02-23 18:21:59 -0500

    nuevo aplicativo Angular

--- ProyectoAngular/src/app/app.component.ts
+++ ProyectoAngular/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Hola Amigos';
+  nombre: string = "";
+  apellido: string = "";
+  colorH3="red";
+  tamanoH3="50px";
+
+  nuevaTienda:any={};
+
+  constructor() {
+    this.nombre = "Mauricio";
+    this.apellido = "Cabrera";
+    console.log("Inicio el constructor");
+  }
+
+  ngOnInit() {
+    this.apellido = "Laguapillo";
+    this.nombre = "Sebastian";
+    console.log("On Init")
+  }
+
+  nombreCompleto(): string {
+    return `${this.nombre} ${this.apellido}`
+  }
+
+  hizoClick() {
+    console.log("Hizo Click");
+  }
+
+  hizoFocus() {
+    console.log("Hizo Focus")
+  }
+
+  crearTienda(formulario){
+    console.log(formulario);
+  }
 }
